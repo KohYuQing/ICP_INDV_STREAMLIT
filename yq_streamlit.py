37c7f0dcfdcf6943a9149d286d6ed459bbda4c80
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-07-31 15:04:36 +0800

    TEST

--- yq_streamlit.py
+++ yq_streamlit.py
@@ -1,3 +1,13 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
-streamlit.title('MY PARENTS NEW DINER')
+st.set_page_config(page_title='INVEMP Tasty Bytes Group 5', page_icon='🍖🍕🍜')
+
+st.sidebar.title("INVEMP: Inventory/Warehouse Management & Prediction on Sales per Menu Item")
+st.sidebar.markdown("This web app allows you to explore the internal inventory of Tasty Bytes. You can explore these functions in the web app (Description of Page)")
+
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Prediction A', 'Prediction B', 'Prediction C', 'Prediction D', 'Prediction E'])
