a678513f7558287d07b69d7629137a7764b4fc9a
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-07-31 15:41:43 +0800

    Update yq_streamlit.py

--- yq_streamlit.py
+++ yq_streamlit.py
@@ -1,28 +1,12 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
+import streamlit 
 import requests
+import pandas 
 import snowflake.connector
 from urllib.error import URLError
-
-st.set_page_config(page_title='INVEMP Tasty Bytes Group 5', page_icon='🍖🍕🍜')
-
-st.sidebar.title("INVEMP: Inventory/Warehouse Management & Prediction on Sales per Menu Item")
-st.sidebar.markdown("This web app allows you to explore the internal inventory of Tasty Bytes. You can explore these functions in the web app (Description of Page)")
-
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['Prediction A', 'Prediction B', 'Prediction C', 'Prediction D', 'Prediction E'])
-
-with tab1:
-  #Tab 1 code here
-  #Hector/Shahid
-with tab2:
-  #Tab 2 code here
-  #Hector/Shahid
-with tab3:
-  #Tab 3 code here
-
-with tab4:
-  #Tab 4 code here
-
-with tab5:
-  #Tab 5 code here
+streamlit.title('My Parents New Healthy Diner')
+streamlit.subheader('Breakfast Favourites')
+streamlit.markdown('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.markdown('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.markdown('🐔 Hard-Boiled Free-Range Egg')
+streamlit.markdown('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
