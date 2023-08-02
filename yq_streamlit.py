55966d99cc8def36f13ced1948457ef5e3b42b40
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-08-02 16:31:44 +0800

    try

--- yq_streamlit.py
+++ yq_streamlit.py
@@ -8,3 +8,5 @@ import pickle
 from sklearn import preprocessing
 
 st.set_page_config(page_title='Singapore Airbnb Price Predictor', page_icon=':money_with_wings:')
+st.sidebar.title("Airbnb Singapore Listings: house (room) prices and locations")
+st.sidebar.markdown("This web app allows you to explore the Airbnb listings in Singapore. You can filter the listings by a price range between $70-180, neighbourhoods and room type. You can also view the listings on a map in the 'Explore' tab and make predictions in the 'Predict' tab.")
