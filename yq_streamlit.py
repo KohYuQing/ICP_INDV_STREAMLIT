991f00ed12dc1364887a5aeee5cd61be95465799
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-08-03 14:15:58 +0800

    zip file

--- yq_streamlit.py
+++ yq_streamlit.py
@@ -6,7 +6,64 @@ import joblib
 from joblib import load
 import pickle
 from sklearn import preprocessing
+import requests
+import zipfile
+import io
 
 st.set_page_config(page_title='Singapore Airbnb Price Predictor', page_icon=':money_with_wings:')
 st.sidebar.title("Airbnb Singapore Listings: house (room) prices and locations")
 st.sidebar.markdown("This web app allows you to explore the Airbnb listings in Singapore. You can filter the listings by a price range between $70-180, neighbourhoods and room type. You can also view the listings on a map in the 'Explore' tab and make predictions in the 'Predict' tab.")
+
+@st.cache_data
+def load_data():
+    # First load the original airbnb listtings dataset
+    data = pd.read_csv("listings.csv") #use this for the original dataset, before transformations and cleaning
+    return data
+
+@st.cache_data
+def load_vis_data():
+    df = pd.read_csv('listings_new2.csv')
+    return data
+
+def read_csv_from_zipped_github(url):
+    # Send a GET request to the GitHub URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Create a BytesIO object from the response content
+        zip_file = io.BytesIO(response.content)
+
+        # Extract the contents of the zip file
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            # Assume there is only one CSV file in the zip archive (you can modify this if needed)
+            csv_file_name = zip_ref.namelist()[0]
+            with zip_ref.open(csv_file_name) as csv_file:
+                # Read the CSV data into a Pandas DataFrame
+                df = pd.read_csv(csv_file)
+
+        return df
+    else:
+        st.error(f"Failed to retrieve data from {url}. Status code: {response.status_code}")
+        return None
+
+def main():
+    st.title("Read CSV from Zipped File on GitHub")
+
+    # Replace the 'github_url' variable with the actual URL of the zipped CSV file on GitHub
+    github_url = "https://github.com/KohYuQing/ICP_INDV_STREAMLIT/blob/main/snowflake_data.zip"
+    df = read_csv_from_zipped_github(github_url)
+
+    if df is not None:
+        # Display the DataFrame in Streamlit
+        st.dataframe(df)
+    else:
+        st.error("Failed to read data from GitHub. Please check the URL and try again.")
+
+if __name__ == "__main__":
+    main()
+
+
+
+data = load_data()
+df = load_vis_data()
