eb082efc6de574e5443cf54d0fbce0163fce2c57
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-08-07 04:04:24 +0800

    index

--- yq_streamlit.py
+++ yq_streamlit.py
@@ -314,7 +314,7 @@ with tab3:
     
     filtered_df = pd.DataFrame(filtered_rows, columns=df.columns)
     bundle_df = filtered_df[filtered_df['VALUE'] != 0]
-    bundle_df = pd.DataFrame(bundle_df)
+    bundle_df = pd.DataFrame(bundle_df, reset_index = True)
     if st.button('Generate Records'):
         st.write(bundle_df)
 
