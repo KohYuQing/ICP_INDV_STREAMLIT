9ae515a032f1579f0f3eb47d9486e5b7cf8b4cf2
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-08-07 17:23:49 +0800

    D

--- yq_streamlit.py
+++ yq_streamlit.py
@@ -134,7 +134,7 @@ with tab3:
     month_input = get_month()
     month_int = month_mapping[month_input]
     
-    Se_column_dtype = city_input.dtype
+    
 
     filtered_rows = []
     for index, row in maintable.iterrows():
@@ -179,15 +179,15 @@ with tab3:
         output_data['DATE_MONTH'] = output_data['DATE_MONTH'].astype(object)
         
         filtered_month = []
-        # for index, row in output_data.iterrows():
-        #     if (month_input in row['DATE_MONTH']):
-        #         filtered_month.append(row)
-        # output_data = pd.DataFrame(filtered_month)
-        # output_data.reset_index(drop=True, inplace=True)
-        date_column_dtype = month_input.dtype
-        ds_column_dtype = output_data['MENU_ITEM_NAME'].dtype
-        st.write(date_column_dtype)
-        st.write(Se_column_dtype)
+        month_input = month_input.astype(str)
+        for index, row in output_data.iterrows():
+            if (month_input in row['DATE_MONTH']):
+                filtered_month.append(row)
+        output_data = pd.DataFrame(filtered_month)
+        output_data.reset_index(drop=True, inplace=True)
+        
+        
+
         st.write(output_data)
         
     # woy2022_df['DATE'] = pd.to_datetime(woy2022_df['DATE'])
