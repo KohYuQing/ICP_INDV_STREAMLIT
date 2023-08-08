30b33866f61ba7fd392ef4bd0d3d8c02a54f6e61
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-08-08 13:37:28 +0800

    emoji

--- yq_streamlit.py
+++ yq_streamlit.py
@@ -25,7 +25,7 @@ with tab2:
   #Hector/Shahid
   st.write('hello')
 with tab3:
-    st.title('Bundling of Items 🍔🍦🥤')
+    st.title('🌭🥤Bundling of Items 🍔🍦')
     @st.cache_data
     def load_data():
     # First load the original airbnb listtings dataset
