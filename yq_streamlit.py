cc8dcc7d051c73ae111b852b6f0370b25dccb68b
KohYuQing <116002595+KohYuQing@users.noreply.github.com>
2023-08-17 00:14:05 +0800

    d

--- yq_streamlit.py
+++ yq_streamlit.py
@@ -319,8 +319,8 @@ with tab3:
             predicted_2022df
 
             merged_df = pd.merge(sum_predicted_2021df, predicted_2022df, on='MENU_TYPE')
-            # difference = ((predicted_2022df['PREDICTED_PRICE']/sum_predicted_2021df['TOTAL_SALES_PER_ITEM']) * 100) - 100
-            merged_df['Percentage_Difference'] = ((predicted_2022df['PREDICTED_PRICE'] - sum_predicted_2021df['TOTAL_SALES_PER_ITEM']) / sum_predicted_2021df['TOTAL_SALES_PER_ITEM']) * 100
+            difference = ((predicted_2022df['PREDICTED_PRICE']/sum_predicted_2021df['TOTAL_SALES_PER_ITEM']) * 100) - 100
+            merged_df['Percentage_Difference'] = difference
 
 
 
